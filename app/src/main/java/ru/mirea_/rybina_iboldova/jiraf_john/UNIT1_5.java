4c459f24adb69f6035a8c919ac9a9eadfc08ba5d
ryybiinana1 <anastasia2z90h@gmail.com>
2024-05-25 00:21:29 +0300

    unit5_new

--- app/src/main/java/ru/mirea_/rybina_iboldova/jiraf_john/UNIT1_5.java
+++ app/src/main/java/ru/mirea_/rybina_iboldova/jiraf_john/UNIT1_5.java
@@ -1,17 +1,23 @@
 package ru.mirea_.rybina_iboldova.jiraf_john;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.RecognitionListener;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 import java.util.Locale;
@@ -19,6 +25,10 @@ import java.util.Locale;
 public class UNIT1_5 extends AppCompatActivity {
 
     private static final int VOICE_RECOGNITION_REQUEST_CODE = 0;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
     private TextView recognizedTextView;
     private SpeechRecognizer speechRecognizer;
     private Intent speechRecognizerIntent;
@@ -37,6 +47,9 @@ public class UNIT1_5 extends AppCompatActivity {
                 startSpeechToText();
             }
         });
+
+        // Запрос разрешений
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
         // Создаем SpeechRecognizer и настраиваем его
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
@@ -80,30 +93,42 @@ public class UNIT1_5 extends AppCompatActivity {
     }
 
     private void startSpeechToText() {
-//        try {
-//            speechRecognizer.startListening(speechRecognizerIntent);
-//        } catch (ActivityNotFoundException e) {
-//            // Если не установлен SpeechRecognizer на устройстве
-//        }
-        try{
+        try {
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                     RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
             intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
             startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
-        }
-        catch(ActivityNotFoundException e)
-        {
-
+        } catch (ActivityNotFoundException e) {
+            // Если не установлен SpeechRecognizer на устройстве
         }
     }
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         if (speechRecognizer != null) {
             speechRecognizer.destroy();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+            ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (matches != null && !matches.isEmpty()) {
+                recognizedTextView.setText(matches.get(0));
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionToRecordAccepted = requestCode == REQUEST_RECORD_AUDIO_PERMISSION && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        if (!permissionToRecordAccepted) {
+            finish();
         }
     }
 }
