ea003dcd0f297f785b277505670416a0ba92bd4e
ryybiinana1 <anastasia2z90h@gmail.com>
2024-05-24 04:36:12 +0300

    Working_Main_lala

--- app/src/main/java/ru/mirea_/rybina_iboldova/jiraf_john/SignUpActivity.java
+++ app/src/main/java/ru/mirea_/rybina_iboldova/jiraf_john/SignUpActivity.java
@@ -3,36 +3,41 @@ package ru.mirea_.rybina_iboldova.jiraf_john;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 
-public class MainActivity1 extends AppCompatActivity {
-    private TextView No_password;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private TextView No_password1;
     private Intent intent;
     private TextView User_name;
     private TextView Password;
     private ConstraintLayout Button_autorizaton;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
-        No_password = findViewById(R.id.no_password);
+        No_password1 = findViewById(R.id.no_password1);
+        No_password1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Здесь код, который будет выполнен при клике на No_password
+                intent = new Intent(SignUpActivity.this, MainActivity1.class);
+                startActivity(intent);
+            }
+        });
         User_name = findViewById(R.id.user_name);
         Password = findViewById(R.id.password);
         Button_autorizaton = findViewById(R.id.button_autorizaton);
 
-       No_password.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Здесь код, который будет выполнен при клике на No_password
-                intent = new Intent(MainActivity1.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
